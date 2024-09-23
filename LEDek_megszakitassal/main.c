fab4b8f952cd1ae2f7610cb712c087980f862bea
RCMD-LaptopB <rcmd>
2024-09-23 16:48:56 +0200

    init

---
+++ LEDek_megszakitassal/main.c
@@ -0,0 +1,19 @@
+/*
+ * LEDek_megszakitassal.c
+ *
+ * Created: 2024-09-23 16:29:42
+ * Author : RCMD-LaptopB
+ */ 
+#define F_CPU 8000000UL // legeslegfelul kell
+#include <avr/io.h>
+
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
