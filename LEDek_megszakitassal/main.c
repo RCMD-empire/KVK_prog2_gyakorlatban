f28f5b7eccc3e00a1c2bbcaea4f5e7b47325474c
RCMD-LaptopB <rcmd>
2024-09-23 17:40:25 +0200

    villognia kéne, de nem villog

--- LEDek_megszakitassal/main.c
+++ LEDek_megszakitassal/main.c
@@ -1,19 +1,50 @@
 /*
  * LEDek_megszakitassal.c
- *
+ * Board: atmega128
  * Created: 2024-09-23 16:29:42
  * Author : RCMD-LaptopB
  */ 
 #define F_CPU 8000000UL // legeslegfelul kell
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
+void port_init(void);
+void timer0_ovr_init(void);
+void LED_out(uint8_t);
+uint8_t led = 0x01, ido = 0;
 
 
 int main(void)
 {
     /* Replace with your application code */
+	port_init();
+	timer0_ovr_init();
     while (1) 
     {
     }
+}
+void port_init(void)
+{
+	DDRB=0xF0;
+	DDRD=0xF0;
+}
+void LED_out(uint8_t ledek)
+{
+	PORTB= (ledek<<4) & 0xF0;
+	PORTD= (ledek) & 0xF0;
+}
+void timer0_ovr_init(void)
+{
+	TCCR0= 1 << CS02 | 0<<CS01 | 0<<CS00; // 1 másodperc
+	TIMSK= 1 << TOIE0;
+	sei();
+}
+ISR(TIMER0_OVF_vect)
+{
+	if (!ido--)
+	{
+		LED_out(led);
+		led = led ^ 0x01;
+	}
 }
 
