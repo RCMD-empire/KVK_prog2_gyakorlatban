ccd5f54c2e18671d209fb25a686d3a2f42d170c6
RCMD-LaptopB <rcmd>
2024-09-30 17:01:07 +0200

    a kijelzők meg lettek mátrixozva! mindenhol 0-ákat

--- LEDek_megszakitassal/main.c
+++ LEDek_megszakitassal/main.c
@@ -11,7 +11,7 @@
 void port_init(void);
 void timer0_ovr_init(void);
 void LED_out(uint8_t);
-uint8_t led = 0x01, ido = 0;
+uint8_t led = 0x01, ido = 0, dig_sel=0, ertek[4]={0};
 
 
 int main(void)
@@ -48,5 +48,8 @@ ISR(TIMER0_OVF_vect)
 		LED_out(led);
 		led = led ^ 0x01;
 	}
+	PORTA = 1<<7 | dig_sel <<4 | ertek[dig_sel];
+	if (dig_sel==3) dig_sel=0;
+	else dig_sel++;
 }
 
