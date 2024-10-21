3b9148b94f1dad3fef7682008a64ecd076e3ecfb
RCMD-LaptopB <rcmd>
2024-10-21 17:07:11 +0200

    ADC init

---
+++ LEDek_megszakitassal/ADC.c
@@ -0,0 +1,6 @@
+/*
+ * ADC.c
+ *
+ * Created: 2024-10-21 17:06:01
+ *  Author: RCMD-LaptopB
+ */ 
---
+++ LEDek_megszakitassal/ADC.h
@@ -0,0 +1,16 @@
+/*
+ * ADC.h
+ *
+ * Created: 2024-10-21 17:05:42
+ *  Author: RCMD-LaptopB
+ */ 
+
+
+#ifndef ADC_H_
+#define ADC_H_
+
+
+
+
+
+#endif /* ADC_H_ */
