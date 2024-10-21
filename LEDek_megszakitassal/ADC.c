4bc57383a4835797f6bc24d4d3da0fefe517344e
RCMD-LaptopB <rcmd>
2024-10-21 17:33:56 +0200

    órán ennyit irtunk, később folytatjuk

--- LEDek_megszakitassal/ADC.c
+++ LEDek_megszakitassal/ADC.c
@@ -7,9 +7,15 @@
 #include "ADC.h"
 void adc_init(void)
 {
-	
+	ADMUX= 0;
+	ADMUX |= 1<< ADLAR;
+	ADCSRA = 0;
+	ADCSRA |= 1<<ADEN ;
 }
 uint16_t adc_conv(uint8_t ch)
 {
-	
+	ADCSRA |= 1<<ADSC;
+	while(!(ADCSRA&ADIF));
+	ADCSRA &= ~(1<<ADSC);
+	return ADC;
 }
