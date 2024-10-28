38bd365ea79a66118bb48212704a4627a3346a03
RCMD-LaptopB <rcmd>
2024-10-28 18:38:26 +0100

    Müködik, pici LED-eken villágyt az érték.

--- LEDek_megszakitassal/ADC.c
+++ LEDek_megszakitassal/ADC.c
@@ -14,9 +14,19 @@ void adc_init(void) {
 }
 uint16_t adc_conv(uint8_t ch) 
 {
-	ADMUX = (ADMUX & 0xE0) (ch & 0x1F);
+	ADMUX = (ADMUX & 0xE0) | (ch & 0x1F);
 	ADCSRA |= 1<<ADSC;
 	while(!(ADCSRA&(1<<ADIF)));
 	//ADCSRA &= ~(1<<ADSC);
 	return ADC;
+}
+uint8_t lm35 (void)
+{
+	uint16_t adc_raw = adc_conv(0);
+	uint8_t temp = 0;
+	uint16_t v_in = 0;
+	uint16_t v_ref = 4096;
+	v_in = (v_ref/1024)*adc_raw;
+	temp = v_in/10;
+	return temp;
 }
