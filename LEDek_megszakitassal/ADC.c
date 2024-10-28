c996d84bb0ad2ebd18df516e16e3dff8c7c0ed42
RCMD-LaptopB <rcmd>
2024-10-28 18:08:31 +0100

    some changes with Döniz's class

--- LEDek_megszakitassal/ADC.c
+++ LEDek_megszakitassal/ADC.c
@@ -9,13 +9,14 @@
 #include "ADC.h"
 void adc_init(void) {
 	ADMUX= 0;
-	ADMUX |= 1<< ADLAR;
-	ADCSRA = 0;
-	ADCSRA |= 1<<ADEN ;
+	//ADMUX |= 1<< ADLAR;
+	ADCSRA = (0 | (1 << ADEN) | (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0));
 }
-uint16_t adc_conv(uint8_t ch) {
+uint16_t adc_conv(uint8_t ch) 
+{
+	ADMUX = (ADMUX & 0xE0) (ch & 0x1F);
 	ADCSRA |= 1<<ADSC;
-	while(!(ADCSRA&ADIF));
-	ADCSRA &= ~(1<<ADSC);
+	while(!(ADCSRA&(1<<ADIF)));
+	//ADCSRA &= ~(1<<ADSC);
 	return ADC;
 }
